3721388187e43044c0b702bb64532ebd04d4f176
lovecosma <williamsmatthew333@gmail.com>
2020-08-23 12:13:58 -0600

    added error messages using fields with errors

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -13,15 +13,16 @@ class ChartsController < ApplicationController
   def create
     @chart = Chart.new
     @chart = current_user.charts.build(chart_params)
-    @chart.birthday = Birthday.find_by(number: @chart.birthday_number)
-    @chart.life_path = LifePath.find_by(number: @chart.life_path_number)
-    @chart.soul_urge = SoulUrge.find_by(number: @chart.soul_urge_number)
-    @chart.soul_urge_challenge = SoulUrgeChallenge.find_by(number: @chart.soul_urge_challenge_number)
-    @chart.expression = Expression.find_by(number: @chart.expression_number)
-    @chart.expression_challenge = ExpressionChallenge.find_by(number: @chart.expression_challenge_number)
-    @chart.personality = Personality.find_by(number: @chart.personality_number)
-    @chart.personality_challenge = PersonalityChallenge.find_by(number: @chart.personality_challenge_number)
-    @chart.save!
+    if @chart.valid?
+      @chart.birthday = Birthday.find_by(number: @chart.birthday_number)
+      @chart.life_path = LifePath.find_by(number: @chart.life_path_number)
+      @chart.soul_urge = SoulUrge.find_by(number: @chart.soul_urge_number)
+      @chart.soul_urge_challenge = SoulUrgeChallenge.find_by(number: @chart.soul_urge_challenge_number)
+      @chart.expression = Expression.find_by(number: @chart.expression_number)
+      @chart.expression_challenge = ExpressionChallenge.find_by(number: @chart.expression_challenge_number)
+      @chart.personality = Personality.find_by(number: @chart.personality_number)
+      @chart.personality_challenge = PersonalityChallenge.find_by(number: @chart.personality_challenge_number)
+    end
     if @chart.save
       redirect_to user_chart_path(current_user, @chart)
     else
@@ -41,6 +42,7 @@ class ChartsController < ApplicationController
   def update
     set_chart
     @chart.update(chart_params)
+    if @chart.valid?
     @chart.birthday = Birthday.find_by(number: @chart.birthday_number)
     @chart.life_path = LifePath.find_by(number: @chart.life_path_number)
     @chart.soul_urge = SoulUrge.find_by(number: @chart.soul_urge_number)
@@ -49,6 +51,7 @@ class ChartsController < ApplicationController
     @chart.expression_challenge = ExpressionChallenge.find_by(number: @chart.expression_challenge_number)
     @chart.personality = Personality.find_by(number: @chart.personality_number)
     @chart.personality_challenge = PersonalityChallenge.find_by(number: @chart.personality_challenge_number)
+    end
     if @chart.save
       redirect_to user_chart_path(current_user, @chart)
     else
